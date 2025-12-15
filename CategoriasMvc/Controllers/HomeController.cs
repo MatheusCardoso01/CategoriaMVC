6bc0cb4d3cc72ea17a1f1ad7287ecbd6719c827f
Matheus Ferreira Cardoso <matheus.cardoso@squadra.com.br>
2025-12-15 09:37:12 -0300

    Versão Iniciar de MVC para consumir minha API Catalogo

---
+++ CategoriasMvc/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using CategoriasMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoriasMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CategoriasMvc/Models/CategoriaViewmodel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CategoriasMvc.Models;
+
+public class CategoriaViewmodel
+{
+    public int CategoriaIa { get; set; }
+
+    [Required(ErrorMessage = "O nome da categoria é obrigatório")]
+    public string? Nome { get; set; }
+
+    [Required]
+    [Display(Name = "Imagem")]
+    public string? ImagemUrl { get; set; }
+}
