80eb1aa06a8b2b74f3502e5890ad427afd42eac2
Matheus Ferreira Cardoso <matheus.cardoso@squadra.com.br>
2025-12-16 15:20:28 -0300

    Desenvolvimento do ProdutosController para consumir a API

--- CategoriasMvc/Controllers/ProdutosController.cs
+++ CategoriasMvc/Controllers/ProdutosController.cs
@@ -1,11 +1,130 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CategoriasMvc.Models;
+using CategoriasMvc.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
 
 namespace CategoriasMvc.Controllers;
 
 public class ProdutosController : Controller
 {
-    public IActionResult Index()
+    private readonly IProdutoService _produtoService;
+    private readonly ICategoriaService _categoriaService;
+    private string token;
+
+    public ProdutosController(IProdutoService produtoService, ICategoriaService categoriaService)
     {
+        _produtoService = produtoService;
+        _categoriaService = categoriaService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProdutoViewModel>>> Index()
+    {
+        // extrai o token do cookie
+        var result = await _produtoService.GetProdutos(ObterTokenJwt());
+
+        if (result is null)
+            return View("Error");
+
+        return View(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> CriarNovoProduto()
+    {
+        var categorias = await CarregaCategorias();
+
+        if (categorias is null)
+            return View("Error");
+
+        ViewBag.Categorias = new SelectList(categorias, "CategoriaId", "Nome");
+
         return View();
     }
+
+    [HttpPost]
+    public async Task<ActionResult<ProdutoViewModel>> CriarNovoProduto(ProdutoViewModel produtoVM)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _produtoService.CriaProduto(produtoVM, ObterTokenJwt());
+
+            if (result is not null)
+                return RedirectToAction(nameof(Index));
+        }
+
+        ViewBag.Erro = "Erro ao criar Produto";
+
+        return View(produtoVM);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ProdutoViewModel>> AtualizarProduto(int id)
+    {
+        var produto = await _produtoService.GetProdutosPorId(id, ObterTokenJwt());
+
+        if (produto is null)
+            return View("Error");
+
+        var categorias = await CarregaCategorias();
+
+        if (categorias is null)
+            return View("Error");
+
+        ViewBag.Categorias = new SelectList(categorias, "CategoriaId", "Nome", produto.CategoriaId);
+
+        return View(produto);
+
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ProdutoViewModel>> AtualizarProduto(ProdutoViewModel produtoVM)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _produtoService.AtualizaProduto(produtoVM.ProdutoId, produtoVM, ObterTokenJwt());
+
+            if (result)
+                return RedirectToAction(nameof(Index));
+        }
+
+        ViewBag.Erro = "Erro ao atualizar Produto";
+
+        return View(produtoVM);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ProdutoViewModel>> VerDetalhesProduto(int id)
+    {
+        var produto = await _produtoService.GetProdutosPorId(id, ObterTokenJwt());
+
+        if (produto is null)
+            return View("Error");
+
+        var categoria = await _categoriaService.GetCategoriaPorId(produto.CategoriaId);
+        ViewBag.CategoriaNome = categoria.Nome;
+
+        return View(produto);
+    }
+
+    // métodos auxiliares
+    private string ObterTokenJwt()
+    {
+        if (HttpContext.Request.Cookies.ContainsKey("X-Access-Token"))
+            token = HttpContext.Request.Cookies["X-Access-Token"].ToString();
+
+        return token;
+    }
+
+    private async Task<IEnumerable<CategoriaViewModel>> CarregaCategorias()
+    { 
+        var categorias = await _categoriaService.GetCategorias();
+
+        if (categorias is null || !categorias.Any())
+            return null;
+
+        return categorias;
+    }
+
 }
--- CategoriasMvc/Services/ProdutoService.cs
+++ CategoriasMvc/Services/ProdutoService.cs
@@ -1,5 +1,6 @@
 ﻿using CategoriasMvc.Models;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 
 namespace CategoriasMvc.Services;
@@ -7,7 +8,7 @@ namespace CategoriasMvc.Services;
 public class ProdutoService : IProdutoService
 {
     private readonly IHttpClientFactory _clientFactory;
-    private const string apiEndpoint = "/api/1/produtos";
+    private const string apiEndpoint = "/api/1/produtos/";
     private readonly JsonSerializerOptions _options;
     private ProdutoViewModel produtoVM;
     private IEnumerable<ProdutoViewModel> produtosVM;
@@ -18,41 +19,118 @@ public class ProdutoService : IProdutoService
         _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true }; // para ignorar maiúsculas e minúsculas
     }
 
-    public Task<IEnumerable<ProdutoViewModel>> GetProdutos(string token)
+    public async Task<IEnumerable<ProdutoViewModel>> GetProdutos(string token)
     {
         var client = _clientFactory.CreateClient("ProdutosApi");
 
         PutTokenInHeaderAuthorization(token, client);
 
+        using (var response = await client.GetAsync(apiEndpoint))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                produtosVM = await JsonSerializer
+                            .DeserializeAsync<IEnumerable<ProdutoViewModel>>
+                            (apiResponse, _options);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        return produtosVM;
 
     }
 
-    public Task<ProdutoViewModel> GetProdutosPorId(int id, string token)
+    public async Task<ProdutoViewModel> GetProdutosPorId(int id, string token)
     {
         var client = _clientFactory.CreateClient("ProdutosApi");
 
         PutTokenInHeaderAuthorization(token, client);
+
+        using (var response = await client.GetAsync(apiEndpoint + id))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                produtoVM = await JsonSerializer
+                            .DeserializeAsync<ProdutoViewModel>
+                            (apiResponse, _options);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        return produtoVM;
     }
 
-    public Task<ProdutoViewModel> CriaProduto(ProdutoViewModel produtoVM, string token)
+    public async Task<ProdutoViewModel> CriaProduto(ProdutoViewModel produtoVM, string token)
     {
         var client = _clientFactory.CreateClient("ProdutosApi");
 
         PutTokenInHeaderAuthorization(token, client);
+
+        var produto = JsonSerializer.Serialize(produtoVM);
+        StringContent content = new StringContent(produto, Encoding.UTF8, "application/json");
+
+        using (var response = await client.PostAsync(apiEndpoint, content))
+        {
+            if (response.IsSuccessStatusCode)
+            { 
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                produtoVM = await JsonSerializer
+                            .DeserializeAsync<ProdutoViewModel>
+                            (apiResponse, _options);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        return produtoVM;
     }
 
-    public Task<bool> AtualizaProduto(int id, ProdutoViewModel produtoVM, string token)
+    public async Task<bool> AtualizaProduto(int id, ProdutoViewModel produtoVM, string token)
     {
         var client = _clientFactory.CreateClient("ProdutosApi");
 
         PutTokenInHeaderAuthorization(token, client);
+
+        using (var response = await client.PutAsJsonAsync(apiEndpoint + id, produtoVM))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 
-    public Task<bool> DeletaProduto(int id, string token)
+    public async Task<bool> DeletaProduto(int id, string token)
     {
         var client = _clientFactory.CreateClient("ProdutosApi");
 
         PutTokenInHeaderAuthorization(token, client);
+
+        using (var response = await client.DeleteAsync(apiEndpoint + id))
+            {
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 
     // métodos auxiliares
